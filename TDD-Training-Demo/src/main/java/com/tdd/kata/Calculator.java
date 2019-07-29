a2b152a43f81af52621ab54a7aa53e45debfd6bd
Vinod Patidar <vinod.patidar1289@gmail.com>
2019-07-29 09:47:28 +0530

    added a method to add values by splitting comma

--- TDD-Training-Demo/src/main/java/com/tdd/kata/Calculator.java
+++ TDD-Training-Demo/src/main/java/com/tdd/kata/Calculator.java
@@ -2,8 +2,24 @@ package com.tdd.kata;
 
 public class Calculator {
 
+	String delimiters = ",";
+
 	public String getMessage() {
 		return "Hello World!";
 	}
-	
+
+	public int addNumbers(String strParams) {
+		int nTotal = 0;
+		try {
+			String strTokenValue[] = strParams.split(delimiters);
+			for(String strValue:strTokenValue) {
+				if(!"".equalsIgnoreCase(strValue)) {
+					nTotal+=Integer.parseInt(strValue);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return nTotal;
+	}
 }
--- TDD-Training-Demo/src/test/groovy/com/tdd/kata/CalculatorSpec.groovy
+++ TDD-Training-Demo/src/test/groovy/com/tdd/kata/CalculatorSpec.groovy
@@ -5,8 +5,20 @@ import spock.lang.Specification
 class CalculatorSpec extends Specification {
 	def objCalculator = new Calculator()
 
-	def 'Get Message'() {
-		expect: 'Should return  the correct message'
+	def 'Welcome Message'() {
+		expect: 'Should return  the welcome message'
 		objCalculator.getMessage() == 'Hello World!'
+	}
+
+	def 'Add numbers from string'(){
+		given: 'A string have number with comma seprated'
+		when: 'Calculator.addNumbers function calls'
+		def nTotal = objCalculator.addNumbers(strParams)
+		then: 'it returns sum of numbers'
+		nTotal == result
+		where:
+		strParams		| 	result
+		""				|	0
+		"3,4"			|	7
 	}
 }
